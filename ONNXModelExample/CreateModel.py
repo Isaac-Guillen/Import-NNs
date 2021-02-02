787e29b60f08c43c59bcb73949239c717b58787a
Isaac-Guillen <52432589+Isaac-Guillen@users.noreply.github.com>
2021-02-02 00:24:28 -0600

    Firs commit

---
+++ ONNXModelExample/CreateModel.py
@@ -0,0 +1,36 @@
+﻿import numpy as np
+import tensorflow as tf
+import keras2onnx as k2o
+import os
+input = tf.keras.Input(shape = (32, 32), dtype = 'float32', name = 'Input')
+
+x = input
+x = tf.keras.layers.Reshape((32, 32, 1))(x)
+x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(x)
+x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(x)
+x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+x = tf.keras.layers.Conv2D(1, (3, 3), activation='relu')(x)
+x = tf.keras.layers.Reshape((4, 4),  name = 'Output')(x)
+
+model = tf.keras.Model(inputs = input, outputs = x)
+
+print(model.summary())
+
+model.compile()
+
+test = np.e * np.ones([1, 32, 32], dtype = 'float32')
+
+result = model(test)
+
+print(result.numpy())
+
+
+#save keras model
+
+model.save(os.getcwd()+"\\myKerasModel")
+
+#save model
+
+onnxModel = k2o.convert_keras(model, 'myOnnxModel', target_opset=7)
+k2o.save_model(onnxModel, os.getcwd()+"\\myModel.onnx")
---
+++ ONNXModelExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace ONNXModelExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "/myModel.onnx";
+            Console.WriteLine(path);
+            Tensor<float> input = new DenseTensor<float>(new[] {32, 32});
+            Tensor<float> output = new DenseTensor<float>(new[] {1, 4, 4});
+            for (int y = 0; y < 32; y++)
+            {
+                for (int x = 0; x < 32; x++)
+                {
+                    input[y, x] = (float)Math.E;
+                }
+            }
+
+            Console.WriteLine(input.GetArrayString());
+
+            // Setup inputs
+            List<NamedOnnxValue> inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("Input", input.Reshape(new [] {1, 32, 32}).ToDenseTensor()),
+            };
+            // Setup outputs
+            List<NamedOnnxValue> outputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("Output", output),
+            };
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            // Run inference
+            InferenceSession session = new InferenceSession(path);
+            session.Run(inputs, outputs);
+            output = outputs[0].AsTensor<float>();
+            Console.WriteLine(output.Reshape(new[] { 4, 4 }).ToDenseTensor().GetArrayString());
+
+            stopWatch.Stop();
+
+            Console.WriteLine(stopWatch.ElapsedMilliseconds.ToString());
+        }
+    }
+}
